d853c7b4cc93c9cbc6ab585bb4169f2583e41304
luiscastcode <cncompusistema@gmail.com>
2025-09-25 13:34:56 -0400

    header mejor

--- app/components/BotonPrincipal.js
+++ app/components/BotonPrincipal.js
@@ -1,7 +1,7 @@
 
 export default function BotonPrincipal({url,nombre}){
 return(
-<a className="mr-4 group relative inline-block focus:ring-3 focus:outline-hidden" href={url}>
+<a className="mr-1 md:mr-4 group relative inline-block focus:ring-3 focus:outline-hidden" href={url}>
   <span
     className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-yellow-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"
   ></span>
